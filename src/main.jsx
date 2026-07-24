31a58115850c4adf4150de31a9d78f19fef877f5
Ombeni Kashutwa <dazzudedon@gmail.com>
2026-07-24 16:33:20 +0300

    update main file and html file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import logoUrl from './assets/logos.png'
+
+// Set browser tab icon dynamically from src/assets/logos.png
+const favicon = document.getElementById('favicon') || document.querySelector("link[rel*='icon']")
+if (favicon) {
+  favicon.href = logoUrl
+  favicon.type = 'image/png'
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
