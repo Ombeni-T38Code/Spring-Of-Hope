c0beda26c5863b569f7f4fe4e5e44b8d8b00791f
Ombeni Kashutwa <dazzudedon@gmail.com>
2026-07-28 08:41:01 +0300

    update index file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css';
 import logoUrl from './assets/logos.png'
 
 // Set browser tab icon dynamically from src/assets/logos.png
